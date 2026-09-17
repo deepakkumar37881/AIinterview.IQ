5ad3e483794c56ed58730e748eb5294689f864ef
Deepak kumar <152618899+deepakkumar37881@users.noreply.github.com>
2026-09-17 23:35:43 +0530

    Update ServerUrl to new backend endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://aiinterview-iq.onrender.com"
+export const ServerUrl  = "https://aiinterview-iq-1backend.onrender.com"
 
 function App() {
 
