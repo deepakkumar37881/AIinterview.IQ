c4befc8201a680e5e9bd06d895ab605a4697a138
Deepak Kumar <deepakraj37881@gmail.com>
2026-09-17 22:36:02 +0530

    First commit

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "aiinterview-4dc3b.firebaseapp.com",
+  projectId: "aiinterview-4dc3b",
+  storageBucket: "aiinterview-4dc3b.firebasestorage.app",
+  messagingSenderId: "678959370193",
+  appId: "1:678959370193:web:8f5aed889bbd6975e19af0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider()
+
+export {auth , provider}
