3e5f45d6f4f897a4aacbefe53e7a5e6f5689884a
Deepak kumar <152618899+deepakkumar37881@users.noreply.github.com>
2026-09-18 01:17:44 +0530

    Rename imported Auth from firebase utils

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -4,7 +4,7 @@ import { IoSparkles } from "react-icons/io5";
 import { motion } from "motion/react"
 import { FcGoogle } from "react-icons/fc";
 import { signInWithPopup } from 'firebase/auth';
-import { auth, provider } from '../utils/firebase';
+import { Auth, provider } from '../utils/firebase';
 import axios from 'axios';
 import { ServerUrl } from '../App';
 import { useDispatch } from 'react-redux';
